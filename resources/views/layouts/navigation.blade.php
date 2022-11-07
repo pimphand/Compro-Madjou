18bd44ccacbb745caa40fed1effe6f0850a9393c
Rochman_arief <rieflvi@gmail.com>
2022-11-07 10:54:45 +0700

    update navigation dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,7 +11,7 @@
         <ul class="nav">
           <li class="nav-item nav-category">Main</li>
           <li class="nav-item">
-            <a href="" class="nav-link">
+            <a href="{{ route('dashboard.index')}}" class="nav-link">
               <i class="link-icon" data-feather="box"></i>
               <span class="link-title">Dashboard</span>
             </a>
