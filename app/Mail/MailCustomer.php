24d58c97a27a2834264e8da4f741ad487e6287f7
Rochman_arief <rieflvi@gmail.com>
2022-11-08 19:36:23 +0700

    update notification & message

---
+++ app/Mail/MailCustomer.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Message;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Address;
+
+class MailCustomer extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $message;
+
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            from: new Address($this->message->email),
+            subject: 'Mail Customer',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'pages.email.index',
+            with: [
+                'text'  => $this->message->text,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
---
+++ resources/views/pages/email/index.blade.php
@@ -0,0 +1,6 @@
+<body>
+ 
+ <h1>Email from customer</h1>
+ <p>{{$text}}</p>
+ 
+</body>
