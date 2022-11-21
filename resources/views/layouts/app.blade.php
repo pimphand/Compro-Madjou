a761e760e02c8037f650ed8676dbd29cb0661f54
Rochman_arief <rieflvi@gmail.com>
2022-11-21 16:34:36 +0700

    edit dan delete blog. note: edit masih harus click 2 kali baru tersimpan. toggle masih ada yg double saat aktif

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,14 +9,9 @@
 
     <title>@yield('title')</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- Plugin css for this page -->
-	<link rel="stylesheet" href="template/assets/vendors/easymde/easymde.min.css">
-	<!-- End plugin css for this page -->
-
-    <!-- Plugin css for this page -->
-	<link rel="stylesheet" href="/template/assets/vendors/prismjs/themes/prism.css">
+	<link rel="stylesheet" href="{{asset('template/assets/vendors/prismjs/themes/prism.css')}}">
 	<!-- End plugin css for this page -->
 
 
@@ -28,28 +23,28 @@
     <!-- End fonts -->
 
     <!-- core:css -->
-    <link rel="stylesheet" href="{{url('template/assets/vendors/core/core.css')}}" />
+    <link rel="stylesheet" href="{{asset('template/assets/vendors/core/core.css')}}" />
     <!-- endinject -->
 
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="template/assets/vendors/flatpickr/flatpickr.min.css" />
+    <link rel="stylesheet" href="{{asset('template/assets/vendors/flatpickr/flatpickr.min.css')}}" />
     <!-- End plugin css for this page -->
 
     <!-- inject:css -->
-    <link rel="stylesheet" href="/template/assets/fonts/feather-font/css/iconfont.css" />
-    <link rel="stylesheet" href="/template/assets/vendors/flag-icon-css/css/flag-icon.min.css" />
+    <link rel="stylesheet" href="{{asset('template/assets/fonts/feather-font/css/iconfont.css')}}" />
+    <link rel="stylesheet" href="{{asset('template/assets/vendors/flag-icon-css/css/flag-icon.min.css')}}" />
     <!-- endinject -->
 
     <!-- Layout styles -->
-    <link rel="stylesheet" href="/template/assets/css/demo1/style.css" />
+    <link rel="stylesheet" href="{{asset('template/assets/css/demo1/style.css')}}" />
     <!-- End layout styles -->
 
-    <link rel="shortcut icon" href="/template/assets/images/favicon.png" />
+    <link rel="shortcut icon" href="{{asset('template/assets/images/favicon.png')}}" />
 
-    <link rel="stylesheet" href="/template/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css">
+    <link rel="stylesheet" href="{{asset('template/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
-    <link rel="stylesheet" href="https://unpkg.com/easymde/dist/easymde.min.css">
+
     
 
 
@@ -62,62 +57,67 @@
     </style>
 </head>
 
-<body class="font-sans antialiased">
+<body>
     <div class="main-wrapper">
-        @include('layouts.navigation')
 
-        <!-- Page Heading -->
-        @if (isset($header))
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
-            </div>
-        </header>
-        @endif
+		<!-- partial:../../partials/_sidebar.html -->
+		@include('layouts.navigation')
+	
+		<div class="page-wrapper">
+				
+			<!-- partial:../../partials/_navbar.html -->
+			@include('layouts.topbar')
+			<!-- partial -->
 
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
-    </div>
+			
+
+                    {{ $slot }}
+
+
+
+			<!-- partial:../../partials/_footer.html -->
+			@include('layouts.footer')
+			<!-- partial -->
+	
+		</div>
+	</div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 
     <!-- core:js -->
-    <script src="template/assets/vendors/core/core.js"></script>
+    <script src="{{asset('template/assets/vendors/core/core.js')}}"></script>
     <!-- endinject -->
 
-    <script src="/template/assets/vendors/prismjs/prism.js"></script>
+    <script src="{{asset('template/assets/vendors/prismjs/prism.js')}}"></script>
 
     <!-- Plugin js for this page -->
-    <script src="/template/assets/vendors/flatpickr/flatpickr.min.js"></script>
-    <script src="/template/assets/vendors/apexcharts/apexcharts.min.js"></script>
+    <script src="{{asset('template/assets/vendors/flatpickr/flatpickr.min.js')}}"></script>
+    <script src="{{asset('template/assets/vendors/apexcharts/apexcharts.min.js')}}"></script>
     <!-- End plugin js for this page -->
 
     <!-- inject:js -->
-    <script src="/template/assets/vendors/feather-icons/feather.min.js"></script>
-    <script src="/template/assets/js/template.js"></script>
+    <script src="{{asset('template/assets/vendors/feather-icons/feather.min.js')}}"></script>
+    <script src="{{asset('template/assets/js/template.js')}}"></script>
     <!-- endinject -->
 
     <!-- Custom js for this page -->
-    <script src="/template/assets/js/dashboard-light.js"></script>
+    <script src="{{asset('template/assets/js/dashboard-light.js')}}"></script>
     <!-- End custom js for this page -->
 
     <!-- Plugin js for this page -->
-    <script src="/template/assets/vendors/datatables.net/jquery.dataTables.js"></script>
-    <script src="/template/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js"></script>
+    <script src="{{asset('template/assets/vendors/datatables.net/jquery.dataTables.js')}}"></script>
+    <script src="{{asset('template/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js')}}"></script>
     <!-- End plugin js for this page -->
 
     {{-- sweet alert --}}
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     {{-- end sweet alert --}}
 
-    <script src="/template/assets/vendors/feather-icons/feather.min.js"></script>
-	<script src="/template/assets/js/template.js"></script>
+    <script src="{{asset('template/assets/vendors/feather-icons/feather.min.js')}}"></script>
+	{{-- <script src="{{asset('template/assets/js/template.js')}}"></script> --}}
 
     <script src="https://unpkg.com/easymde/dist/easymde.min.js"></script>
     <script src="https://cdn.ckeditor.com/ckeditor5/23.0.0/classic/ckeditor.js"></script>
-    {{-- <script src="/template/assets/vendors/core/perfect-scrollbar.min.js.map"></script> --}}
 
     @stack('js')
 
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="sidebar">
+<nav class="sidebar">
     <div class="sidebar-header">
         <a href="{{route('dashboard.index')}}" class="sidebar-brand"> Madjou <span>Web</span> </a>
         <div class="sidebar-toggler not-active">
@@ -16,6 +16,7 @@
               <span class="link-title">Dashboard</span>
             </a>
           </li>
+          
           <li class="nav-item nav-category">Notifikasi dan langganan</li>
           
           <li class="nav-item">
@@ -32,16 +33,16 @@
           </li>
 
           <li class="nav-item nav-category">Layanan</li>
+
           <li class="nav-item">
             <a href="{{ route('services.index')}}" class="nav-link">
               <i class="link-icon" data-feather="tool"></i>
               <span class="link-title">Layanan</span>
             </a>
           </li>
-
           <li class="nav-item">
             <a href="{{ route('detail-services.index')}}" class="nav-link">
-              <i class="link-icon" data-feather="tool"></i>
+              <i class="link-icon" data-feather="bell"></i>
               <span class="link-title">Detail layanan</span>
             </a>
           </li>
@@ -143,112 +144,7 @@
 
   <!-- partial -->
 
-  <div class="page-wrapper">
+ 
     <!-- partial:partials/_navbar.html -->
-    <nav class="navbar">
-      <a href="#" class="sidebar-toggler">
-        <i data-feather="menu"></i>
-      </a>
-      <div class="navbar-content">
-        <ul class="navbar-nav">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="languageDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="flag-icon flag-icon-us mt-1" title="us"></i> <span class="ms-1 me-1 d-none d-md-inline-block">English</span>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="languageDropdown">
-              <a href="javascript:;" class="dropdown-item py-2"><i class="flag-icon flag-icon-us" title="us" id="us"></i> <span class="ms-1"> English </span></a>
-              <a href="javascript:;" class="dropdown-item py-2"><i class="flag-icon flag-icon-id" title="fr" id="fr"></i> <span class="ms-1"> Indonesian </span></a>
-            </div>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="appsDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i data-feather="grid"></i>
-            </a>
-            <div class="dropdown-menu p-0" aria-labelledby="appsDropdown">
-              <div class="px-3 py-2 d-flex align-items-center justify-content-between border-bottom">
-                <p class="mb-0 fw-bold">Web Apps</p>
-                <a href="javascript:;" class="text-muted">Edit</a>
-              </div>
-              <div class="row g-0 p-1">
-                <div class="col-3 text-center">
-                  <a href="pages/apps/chat.html" class="dropdown-item d-flex flex-column align-items-center justify-content-center wd-70 ht-70"
-                    ><i data-feather="message-square" class="icon-lg mb-1"></i>
-                    <p class="tx-12">Chat</p></a
-                  >
-                </div>
-                <div class="col-3 text-center">
-                  <a href="pages/apps/calendar.html" class="dropdown-item d-flex flex-column align-items-center justify-content-center wd-70 ht-70"
-                    ><i data-feather="calendar" class="icon-lg mb-1"></i>
-                    <p class="tx-12">Calendar</p></a
-                  >
-                </div>
-                <div class="col-3 text-center">
-                  <a href="pages/email/inbox.html" class="dropdown-item d-flex flex-column align-items-center justify-content-center wd-70 ht-70"
-                    ><i data-feather="mail" class="icon-lg mb-1"></i>
-                    <p class="tx-12">Email</p></a
-                  >
-                </div>
-                <div class="col-3 text-center">
-                  <a href="pages/general/profile.html" class="dropdown-item d-flex flex-column align-items-center justify-content-center wd-70 ht-70"
-                    ><i data-feather="instagram" class="icon-lg mb-1"></i>
-                    <p class="tx-12">Profile</p></a
-                  >
-                </div>
-              </div>
-              <div class="px-3 py-2 d-flex align-items-center justify-content-center border-top">
-                <a href="javascript:;">View all</a>
-              </div>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('messages.index')}}">
-              <i data-feather="mail"></i>
-            </a>
-          </li>
-         
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <img class="wd-30 ht-30 rounded-circle" src="https://via.placeholder.com/30x30" alt="profile" />
-            </a>
-            <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
-              <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
-                <div class="mb-3">
-                  <img class="wd-80 ht-80 rounded-circle" src="https://via.placeholder.com/80x80" alt="" />
-                </div>
-                <div class="text-center">
-                  <p class="tx-16 fw-bolder">{{ Auth::user()->name }}</p>
-                </div>
-              </div>
-              <ul class="list-unstyled p-1">
-                <li class="dropdown-item py-2">
-                  <a href="pages/general/profile.html" class="text-body ms-0">
-                    <i class="me-2 icon-md" data-feather="user"></i>
-                    <span>Profile</span>
-                  </a>
-                </li>
-                <li class="dropdown-item py-2">
-                  <a href="javascript:;" class="text-body ms-0">
-                    <i class="me-2 icon-md" data-feather="edit"></i>
-                    <span>Edit Profile</span>
-                  </a>
-                </li>
-               
-                <div class="d-flex align-item-center justify-content-center px-5 py-3">
-                
-                  <!-- Authentication -->
-                  <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                        <button class="btn btn-primary">
-                            <i class="me-2 icon-md" data-feather="log-out"></i>
-                            <span>Log Out</span>
-                        </button>
-                    </form>
-                
-                </div>
-              </ul>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
+  
     <!-- partial -->
