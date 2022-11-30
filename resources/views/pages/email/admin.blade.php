4da9bdae4faf7399ebed964ab491f35cc5d955b1
Rochman_arief <rieflvi@gmail.com>
2022-11-30 12:49:02 +0700

    view email dan plugin xendit

--- resources/views/pages/email/admin.blade.php
+++ resources/views/pages/email/admin.blade.php
@@ -1,7 +1,13 @@
 <body>
- 
- <h1>Email from customer</h1>
- <h4>{{$subject}}</h4>
- <p>{{$comment}}</p>
- 
+     <div class="header" style="background-color: #fddea7; width:100%; height:50px; padding:0px">
+        <img src="{{asset('template/madjouLogo.png')}}" height="40px" width="200px" style="padding: 5px" alt=""> 
+    </div>
+    <div class="mail-body">
+        <h1 style="text-align: center;">Email To customer</h1>
+        <h4>Subject : {{$subject}}</h4>
+        <div class="content" style="text-indent: 50px">
+            {!! htmlspecialchars_decode($comment) !!}
+        </div>
+    </div>
+
 </body>
