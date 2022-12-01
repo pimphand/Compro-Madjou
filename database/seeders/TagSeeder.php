de4f78895750fd59a6d30e42317083584139398a
pimphand <faisaldwiki69@gmail.com>
2022-12-01 19:13:31 +0700

    add seeder

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TagSeeder extends Seeder
 {
@@ -14,6 +15,12 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create('id_ID');
+        for ($i = 1; $i <= 50; $i++) {
+            DB::table('tags')->insert([
+                'type' => $faker->randomElement(['all', 'blog', 'service']),
+                'name' => $faker->jobTitle,
+            ]);
+        }
     }
 }
